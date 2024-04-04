0772e8294955b647f5673d8285a92683ddfa1d4e
Iskandar <isikjandev@gmail.com>
2024-04-04 19:26:46 +0500

    101%

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let menu = $('.navbar__mob-body');
+$('.open').on("click", function () {
+    menu.toggleClass('navbar__mob-body-active');
+});
+$('.close').on("click", function () {
+    menu.toggleClass('navbar__mob-body-active');
+});
