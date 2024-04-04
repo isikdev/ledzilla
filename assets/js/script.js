7e00989103c9b0dc3af708c0bd9be2b3f1ef6c30
Iskandar <isikjandev@gmail.com>
2024-04-04 19:40:19 +0500

    101%

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,33 @@ $('.open').on("click", function () {
 $('.close').on("click", function () {
     menu.toggleClass('navbar__mob-body-active');
 });
+
+$(document).ready(function () {
+    $('#carBrand').on('input', function () {
+        var inputText = $(this).val().trim();
+        var suggestions = $('#carBrandSuggestions');
+
+        if (inputText.length > 0) {
+            $.ajax({
+                url: 'https://api.cars-base.ru/cars',
+                method: 'GET',
+                data: {
+                    full: 1
+                },
+                dataType: 'json',
+                success: function (data) {
+                    suggestions.empty(); // Очищаем предыдущие подсказки
+                    $.each(data, function (index, item) {
+                        var suggestion = $('<div>').text(item['mark-name']).addClass('suggestion');
+                        suggestions.append(suggestion); // Выводим новые подсказки
+                    });
+                },
+                error: function (xhr, status, error) {
+                    console.error('Ошибка при получении подсказок:', error);
+                }
+            });
+        } else {
+            suggestions.empty(); // Очищаем подсказки, если поле ввода пустое
+        }
+    });
+});
